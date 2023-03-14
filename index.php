11dc0adf5d436113340c7e778a0db76e5da0d3e6
Peter Chen <peterchen2017125@gmail.com>
2023-03-14 10:19:21 +0800

    20230314

--- index.php
+++ index.php
@@ -50,8 +50,20 @@
             </div>
         </div>
     </nav>
-    <div class="container" style="margin-top: 86px";>
-
+    <div class="container" style="margin-top: 86px" ;>
+        <div class="row justify-content-between align-items-center">
+            <h5 class="text-center text-white border-start font-weight-bolder">商品展示</h5>
+            <form id="search-product" class="d-flex justify-content-end align-items-center" action="search_product.php"
+                method="post">
+                <input type="search" name="search" id="search-input" class="form-control w-25 mx-1"
+                    placeholder="請輸入商品名稱">
+                <input type="number" name="min_price" id="min-price-input" class="form-control w-25 mx-1"
+                    placeholder="最低價格">
+                <input type="number" name="max_price" id="max-price-input" class="form-control w-25 mx-1"
+                    placeholder="最高價格">
+                <button type="submit" class="btn btn-secondary">查詢</button>
+            </form>
+        </div>
     </div>
 </body>
 <script src="./js/jquery-3.6.3.min.js"></script>
